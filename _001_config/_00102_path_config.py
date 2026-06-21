ea144e24b024843af1e030bdc93249a74c8ded85
Nightminhster <trinhminhhieu003@gmail.com>
2026-06-21 18:09:29 +0700

    update path config

--- _001_config/_00102_path_config.py
+++ _001_config/_00102_path_config.py
@@ -1,6 +1,6 @@
 from pathlib import Path 
 import os 
 data_path = {
-    'root_path': str(Path("D:\\risk_analyst\\_004_store_data")) + os.sep,
-    'warehouse_path': str(Path("E:\\Risk Analyst\\warehouse_data")) + os.sep,
+    'root_path': str(Path("")) + os.sep,
+    'warehouse_path': str(Path("")) + os.sep,
 }
